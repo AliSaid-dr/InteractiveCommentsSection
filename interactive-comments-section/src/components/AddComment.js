ef4cc0912ba66dda1f7041d9c77158f4104e5df4
ali <ali.osman1@abv.bg>
2022-10-01 19:33:03 +0300

    bugs fixed

--- interactive-comments-section/src/components/AddComment.js
+++ interactive-comments-section/src/components/AddComment.js
@@ -1,15 +1,13 @@
 function AddComment() {
     return (
-        <div className="add-comment-container">
-            <form action="">
-                <div className="comment-text">
-                    <textarea name="comment" id="comment" cols="41" rows="5" className="comment" placeholder = "Add a comment..."></textarea>
-                </div>
-                <div className="comment-item">
+        <div className="container">
+            <div className="add-comment-container">
+                <form action="" className="comment-form">
+                    <textarea name="comment" id="comment" rows="6" className="comment" placeholder = "Add a comment..."></textarea>
                     <img src="/images/avatars/image-juliusomo.png" className="avatar-image" alt="Avatar" />
                     <input className="send" type="submit" value="send"/>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     )
 }
