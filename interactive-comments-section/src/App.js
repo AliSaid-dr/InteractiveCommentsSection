4ba4abf846b350c691a8a7c9b61d37caedfd33c8
AliSaid-dr <ali_said@abv.bg>
2022-10-08 18:42:45 +0300

    comment section added

--- interactive-comments-section/src/App.js
+++ interactive-comments-section/src/App.js
@@ -1,10 +1,23 @@
+import {useState, useEffect} from 'react';
 import Comments from './components/Comments';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('data.json')
+      .then(res => {
+        setData(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  })
   return (
     <div>
-      
+      <Comments data={data}/>
     </div>
   );
 }
